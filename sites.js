0f4f56ff273fc60a6dd72ad7a9ccc76e1a5bed72
Mark Fernandes <mfernandes61@gmail.com>
2018-12-17 12:03:46 +0000

    Create sites.js
    Javascript holding sites to be opened
    (Change this to the ones you want to appear)

---
+++ sites.js
@@ -0,0 +1,4 @@
+<script type="text/javascript">
+	links = ["https://bioinformatics-core-shared-training.github.io/2018-11-29-Crick-London/",
+	"https://pad.carpentries.org/2018-11-29-Crick-London"];
+</script>
